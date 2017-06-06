d8f45219363de31d78b3d1f885a118cb57744946
Kurtis Reid <kreid@unionhomemortgage.com>
2017-06-06 16:18:25 -0400

    changed to bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PicturesComponent,
+    AboutMeComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NavbarComponent]
 })
 export class AppModule { }
